46678556cbef39edf314eeebfe721ff387a0e8d7
Anwesh43 <anweshthecool0@gmail.com>
2017-03-10 18:50:43 +0530

    making some minor modification

--- container_components/addbutton.js
+++ container_components/addbutton.js
@@ -1,7 +1,7 @@
 import addBlock from '../actions/create_block'
 import {connect} from 'react-redux'
 import React,{PropTypes} from 'react'
-const AddButton = (dispatch)=>{
+const AddButton = ({dispatch})=>{
     var onClick = ()=>{
         dispatch(addBlock(this.refs.t1.value))
     }
@@ -9,9 +9,6 @@ const AddButton = (dispatch)=>{
       <input type='text' ref='t1'/>
       <button onClick={onClick}>suBMIT</button>
     </div>
-}
-AddButton.propTypes = {
-    dispatch:propTypes.func.isRequired
 }
 AddButton = connect()(AddButton)
 export default AddButton
