de42edf8209b069f3d56bde2973fc6eefa7c9aaa
Anwesh43 <anweshthecool0@gmail.com>
2017-03-10 20:55:28 +0530

    changing i to action.index

--- reducers/block_app.js
+++ reducers/block_app.js
@@ -1,12 +1,16 @@
+const color = "red"
 const addBlockReducer = (state={blocks:[]},action)=> {
-    var newState = Object.assign({},state)
+    var newState = {}
+    for (var key in state) {
+        newState[key] = state[key]
+    }
     var blocks = newState.blocks
     if(action.type == "ADD") {
         blocks.push({bg:'white',text:action.text})
     }
     if(action.type == "CHANGE") {
-        if(i<blocks.length) {
-            blocks[action.index].bg = action.color
+        if(action.index<blocks.length) {
+            blocks[action.index].bg = (blocks[action.index].bg == "white")?"red":"white"
         }
     }
     return newState
