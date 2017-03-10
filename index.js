be27d98953ff5587dc62aeeb681d219d1bc7f286
Anwesh43 <anweshthecool0@gmail.com>
2017-03-10 23:55:43 +0530

    Combining two reducers

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-import {createStore} from 'redux'
+import {createStore,combineReducers} from 'redux'
 import {Provider} from 'react-redux'
 import React from 'react'
 import {render} from 'react-dom'
 import AddButton from './container_components/addbutton'
 import VisibleBlockList from './container_components/visible_block_list'
 import addBlockReducer from './reducers/block_app'
-const store = createStore(addBlockReducer)
-store.subscribe(()=>{console.log(store.getState())})
+import PickColorComponent from './container_components/pick_color_component'
+import colorReducer from './reducers/color_reducer'
+const store = createStore(combineReducers({color:colorReducer,blocks:addBlockReducer}))
+
 const App = ()=>{
     return <Provider store={store}>
       <div>
           <VisibleBlockList/>
+          <PickColorComponent/>
           <AddButton/>
       </div>
     </Provider>
